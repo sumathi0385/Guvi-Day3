25199704406f17bfff101bd7f24c1d4ef9343238
sumathi0385 <sumathi0385@gmail.com>
2022-10-28 00:32:25 +0530

    Added new file

---
+++ resumeInJSON.js
@@ -0,0 +1,60 @@
+var resumeData = { "Sumathi_Resume": 
+                    [{"Personal Details":
+                                   [
+                                    {"Name"          : "Sumathi"},
+                                    {"Father's Name" : "Chelladurai"} ,
+                                    {"Husband Name"  : "Edwin"},
+                                    {"Qualification" : "BE - ECE"},
+                                    {"Address"       : "No 1 First Street Bharathi Nagar Jamin Pallavaram Chennai-43"},
+                                    {"Email"         : "sumathi0385@gmail.com"},
+                                    {"Phone"         : "9875564567"}
+                                   ]   
+
+                    },                     
+                    
+                    {
+                        "Objective"     : "To pursue a challenging and growth-oriented career in IT consulting and wants to be a core member of an organization where my skills will have a valuable application",
+                        "Experience summary"    : "Completed CTFL and Scrum Master certification, 10+ years of experience in testing field, 3+ years in Automation field",
+                    },
+
+                    { "Qualification" :
+                                    [ { "Degree" : "BE - ECE", "Year" : "2006"},
+                                      { "Degree" : "12th",     "Year" : "2002"},
+                                      { "Degree" : "10th",     "Year" : "2000"}
+
+                                    ]
+
+                    },
+                    { "Technical Skills" :
+                                    [ { "Functional Tool"       : "Selenium, SOAP UI"},
+                                      { "Test Management Tool"  : "Quality Center"},
+                                      { "Defect Tracking Tool"  : "TFS, Bug Tracker, JIRA"},
+                                      { "DataBase"              : "SQL"},
+                                      { "Language"              : "HTML, Core JAVA"}
+
+                                    ]
+                    },
+                    { "Project Details":
+                                    [{"Project Name": "Costco Travel", "Technology": "JAVA, JSP Servelet"},
+                                     {"Project Name": "MissionBell",   "Technology": "DOT NET, SQL"},
+                                     {"Project Name": "Global Affluent", "Technology": "JAVA, Android, IOS"}
+
+                                    ]
+
+                    },
+                    { "Professional Experience":
+                                    [{"Date": "Oct 2013 = Til date", "Organization": "Smartek21", "Role": "Lead QA", "Place": "Chennai"},
+                                     {"Date": "Aug 2012 = TOct 2013", "Organization": "Xchanging", "Role": "Senior QA", "Place": "Bangalore"},
+                                     {"Date": "Nov 2009 = Aug 2012", "Organization": "Smartek21", "Role": "QA Engineer", "Place": "Chennai"},
+                                     {"Date": "May 2007 = Oct 2009", "Organization": "Precosion", "Role": "Trainee Engineer", "Place": "Chennai"}
+
+                                    ]
+
+                    }
+                
+                
+                
+                
+                ]       
+
+}
